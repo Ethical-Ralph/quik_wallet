310351c93f1bc039f2f15ceafa030cd384be017a
EthicalRalph <rakinola90@gmail.com>
2022-04-13 19:27:06 +0100

    update: disabled gorm logger

--- infrastructure/database/database.go
+++ infrastructure/database/database.go
@@ -5,10 +5,13 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func Connect(connectionString string) (*gorm.DB, error) {
-	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 
 	if err != nil {
 		return nil, err
